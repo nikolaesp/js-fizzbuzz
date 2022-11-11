ae37289bdaea251ec060303d010ddeb42fe6c7f4
Nikola Krstevski <36802068+nikolaesp@users.noreply.github.com>
2022-11-11 15:10:05 +0100

    seconda prova

--- script.js
+++ script.js
@@ -5,15 +5,15 @@ let containerPrincipale = document.querySelector(".mio-container");
 for(let i=1;i<101;i++){
 
     if(i%15==0 )//se e devisibile con 3 e con 5 stampa fizzbuzz
-    containerPrincipale.innerHTML += '<div class="square"><p>FizzBuzz</p></div>';
+    containerPrincipale.innerHTML += `<div class="square"><p>FizzBuzz</p></div>`;
     else
     if(i%3==0) //se e devisibile con 3 stampa fizz
-    containerPrincipale.innerHTML += '<div class="square"><p>Fizz</p></div>';
+    containerPrincipale.innerHTML += `<div class="square"><p>Fizz</p></div>`;
     else
     if(i%5==0) //se e devisibile con 5 stampa fizz
-    containerPrincipale.innerHTML += '<div class="square"><p>Buzz</p></div>';
+    containerPrincipale.innerHTML += `<div class="square"><p>Buzz</p></div>`;
     else
-    containerPrincipale.innerHTML += '<div class="square"><p>${i}</p></div>'; //altrimente stampa il numero
+    containerPrincipale.innerHTML += `<div class="square"><p>${i}</p></div>`; //altrimente stampa il numero
 
     
 
