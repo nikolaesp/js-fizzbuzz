dadeb5e1a1464cf9273725279de05c1dc41a4d78
Nikola Krstevski <36802068+nikolaesp@users.noreply.github.com>
2022-11-11 14:58:54 +0100

    fatta logica

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
 
-for(let i=0;i<100;i++){
-    if(i%3==0){ //se e devisibile con 3 stampa fizz
+for(let i=1;i<101;i++){
+
+    if(i%15==0 )//se e devisibile con 3 e con 5 stampa fizzbuzz
+    console.log("FizzBuzz");
+    else
+    if(i%3==0) //se e devisibile con 3 stampa fizz
         console.log("Fizz");
-    }
-    if(i%5==0){ //se e devisibile con 5 stampa fizz
+    else
+    if(i%5==0) //se e devisibile con 5 stampa fizz
         console.log("Buzz");
-    }else{
-        console.log(i);
-    }
+    else
+        console.log(i); //altrimente stampa il numero
+    
 
 }
