c88fd629239416f340acdfcb751e40a2b27baaee
Nikola Krstevski <36802068+nikolaesp@users.noreply.github.com>
2022-11-11 14:53:14 +0100

    logica

---
+++ script.js
@@ -0,0 +1,12 @@
+
+for(let i=0;i<100;i++){
+    if(i%3==0){ //se e devisibile con 3 stampa fizz
+        console.log("Fizz");
+    }
+    if(i%5==0){ //se e devisibile con 5 stampa fizz
+        console.log("Buzz");
+    }else{
+        console.log(i);
+    }
+
+}
